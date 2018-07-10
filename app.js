eaf729392d9f2cded687e9564b25e198aba09115
Damola <damola.adekoya@lagetronix.com>
2018-07-10 19:42:03 +0100

    Error fixed

--- app.js
+++ app.js
@@ -721,6 +721,7 @@ function greetUserText(userId) {
 
 function getUser(userId) {
     //first read user firstname
+    var user = null;
     request({
         uri: 'https://graph.facebook.com/v2.7/' + userId,
         qs: {
@@ -730,12 +731,12 @@ function getUser(userId) {
     }, function(error, response, body) {
         if (!error && response.statusCode == 200) {
 
-            var user = JSON.parse(body);
+            user = JSON.parse(body);
 
             if (user.first_name) {
                 console.log("FB user: %s %s, %s",
                     user.first_name, user.last_name, user.gender);
-                return user;
+
 
             } else {
                 console.log("Cannot get data for fb user with id",
@@ -746,6 +747,7 @@ function getUser(userId) {
         }
 
     });
+    return user;
 }
 
 /*
